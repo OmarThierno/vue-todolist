58935f13a6af0ad377b7124a6d208443d1ec14e6
OmarThierno <thiernomarndiaye76@gmail.com>
2024-04-19 15:51:33 +0200

    feat MILESTONE 1

---
+++ js/script.js
@@ -0,0 +1,46 @@
+
+
+const {createApp} = Vue;
+
+createApp({
+  data() {
+    return {
+      list: [
+        {
+          task: 'Leggere la letterina di Babbo natale',
+          isDone: false,
+        },
+        {
+          task: 'Comprare il latte',
+          isDone: false,
+        },
+        {
+          task: 'Dar da mangiare al Gatto',
+          isDone: true,
+        },
+        {
+          task: 'Fare il bucato',
+          isDone: false,
+        },
+        {
+          task: 'Sconfiggere Lori a scacchi',
+          isDone: false,
+        },
+      ],
+      newTask: {
+        task: '',
+        isDone: false
+      },
+      user: '',
+    }
+  },
+  created() {
+
+  }, 
+  methods: {
+    addTask: function() {
+      this.list.push({...newTask})
+    }
+  }
+}).mount('#app')
+
