261e7539a5fb061c609581296a591049fab7b06f
OmarThierno <thiernomarndiaye76@gmail.com>
2024-04-20 22:24:34 +0200

    feat: add last bonus

--- vue-methods/js/script.js
+++ vue-methods/js/script.js
@@ -35,18 +35,29 @@ createApp({
     }
   },
   created() {
+    const localList = localStorage.getItem('list');
 
+    if (localList !== null) {
+      this.list = JSON.parse(localList)
+    } else {
+      this.list = this.list;
+    }
   }, 
   methods: {
     addTask: function() {
       if (this.newTask.task !== '') {
         this.list.push({...this.newTask});
         this.newTask.task = '';
+
+        const jsonList = JSON.stringify(this.list);
+        console.log(jsonList);
+
+        localStorage.setItem('list', jsonList);
       }
     },
     removeTask: function(indexToRemove) {
       this.list.splice(indexToRemove, 1);
-    }
+    },
   }
 }).mount('#app')
 
