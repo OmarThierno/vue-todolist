881894e13e4344c09028676f819c2a5e6e94d726
OmarThierno <thiernomarndiaye76@gmail.com>
2024-04-21 14:57:55 +0200

    feat: bonust 2 with without vue js

--- without-vue-js/js/function.js
+++ without-vue-js/js/function.js
@@ -2,12 +2,19 @@ function createElem(curObject) {
   const liElem = document.createElement('li');
   const spanElem = document.createElement('span');
   spanElem.innerHTML = curObject.task;
-  spanElem.classList.add('list-of-list')
+  spanElem.classList.add('list-of-list');
 
   if (curObject.isDone) {
     spanElem.classList.add('strikethrough-text');
   }
 
+  liElem.addEventListener('click', () => {
+    console.log('click')
+    spanElem.classList.toggle('strikethrough-text')
+    curObject.isDone = !curObject.isDone;
+    console.log(list);
+  })
+  
   liElem.append(spanElem);
   listElem.append(liElem);
 }
